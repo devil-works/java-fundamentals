d2325c145abd8af25c4e568c39c8f1faa42573b6
devil-works <tkhr6022@gmail.com>
2023-01-02 15:10:01 +0900

    first commit

---
+++ src/practice/DataType.java
@@ -0,0 +1,29 @@
+package practice;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		byte cars = 5;
+		System.out.println(cars);
+		
+		short myAge = 29;
+		System.out.println(myAge);
+
+		int mySalary = 300000;
+		System.out.println(mySalary);
+
+		long populationJapan = 125_502_290;
+		System.out.println(populationJapan);
+
+		boolean isExist = true;
+		System.out.println(isExist);
+		
+		char alphabet = 'A';
+		System.out.println(alphabet);
+
+		String productDiscription = "This is a pen";
+		System.out.println(productDiscription);
+
+	}
+
+}
---
+++ src/practice/Variable.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 29;
+		System.out.println(age);
+		
+		int thisYear = 2022;
+		System.out.println(thisYear);
+		
+	}
+
+}
